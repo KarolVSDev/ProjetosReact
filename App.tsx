713b876624f9742639b304dfd04dc4d52af4522d
Ana Karoline <anadevkarol@gmail.com>
2024-09-10 21:41:27 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // Função para lidar com o clique do botão
+  const handlePress = () => {
+    console.log("Botão clicado!");
+    Alert.alert("Confirmação", "Você confirmou que quer aprender!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Eu estou aprendendo React Native</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Vamos aprender juntos?</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +26,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
